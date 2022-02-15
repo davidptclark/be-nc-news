dc9134c3268fb98629c32def105bb4c467f05a94
David Clark <david.p.t.clark@gmail.com>
2022-02-15 15:46:23 +0000

    Add refactored articles.models.js and resolved fatal error in testing

--- models/articles.model.js
+++ models/articles.model.js
@@ -2,7 +2,10 @@ const db = require('../db/connection');
 
 exports.fetchArticleById = (id) => {
   return db
-    .query('SELECT * FROM articles WHERE article_id = $1;', [id])
+    .query(
+      'SELECT articles.*, COUNT(comments) AS comment_count FROM comments LEFT JOIN articles ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id;',
+      [id]
+    )
     .then(({ rows: article }) => {
       if (article.length === 0) {
         return Promise.reject({ status: 404, msg: 'article not found' });
