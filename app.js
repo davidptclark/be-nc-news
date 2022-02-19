1851566dd873739caeceeb203bcc5832df253a29
David Clark <david.p.t.clark@gmail.com>
2022-02-19 14:18:37 +0000

    Refactor to use express routing for all endpoints

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
 const express = require('express');
-const { getEndpoints } = require('./controllers/api.controller');
-const { getTopics } = require('./controllers/topics.controller');
-const { getUsers } = require('./controllers/users.controller');
-const {
-  getArticleById,
-  patchVotesByID,
-  getArticles,
-} = require('./controllers/articles.controller');
-const {
-  getCommentsByArticleId,
-  postCommentsByArticleId,
-  deleteCommentById,
-} = require('./controllers/comments.controller');
 const {
   handle404,
   handle500,
   handlePsqlErrors,
   handleCustomErrors,
 } = require('./controllers/errors.controller');
+const apiRouter = require('./routes/api-router');
 
 const app = express();
+
 app.use(express.json());
 
-app.get('/api', getEndpoints);
-
-app.get('/api/topics', getTopics);
-
-app.get('/api/articles', getArticles);
-app.get('/api/articles/:article_id', getArticleById);
-app.patch('/api/articles/:article_id', patchVotesByID);
-
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
-app.post('/api/articles/:article_id/comments', postCommentsByArticleId);
-
-app.get('/api/users', getUsers);
-
-app.delete('/api/comments/:comment_id', deleteCommentById);
+app.use('/api', apiRouter);
 
 app.all('/*', handle404); //Using app.all to apply this controller function to all bad paths
 app.use(handleCustomErrors);
