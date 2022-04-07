56bf6bb839c029766db9a7f65a08cfd3e042cdae
David Clark <david.p.t.clark@gmail.com>
2022-04-07 11:33:42 +0100

    Add endpoints to GET user by username

--- models/users.model.js
+++ models/users.model.js
@@ -1,7 +1,28 @@
-const db = require('../db/connection');
+const db = require("../db/connection");
 
 exports.fetchUsers = async () => {
-  const { rows: users } = await db.query('SELECT username FROM users;');
+  const { rows: users } = await db.query("SELECT username FROM users;");
 
   return users;
+};
+
+// exports.fetchUserByUsername = async (username) => {
+//     const rows = await db.query('SELECT * FROM users WHERE username = $1;' [username]);
+
+//     if (rows.length === 0) {
+//       return Promise.reject({ status: 404, msg: 'username does not exist' });
+//     }
+
+//     return rows
+// }
+
+exports.fetchUserByUsername = (username) => {
+  return db
+    .query("SELECT * FROM users WHERE username = $1;", [username])
+    .then(({ rows: user }) => {
+      if (user.length === 0) {
+        return Promise.reject({ status: 404, msg: "username not found" });
+      }
+      return user;
+    });
 };
